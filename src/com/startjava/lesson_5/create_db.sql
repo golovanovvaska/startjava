1adee14fdb4590c4a33d5f5929031286db65108b
vasiliy <golovanovvaska@gmail.com>
2023-08-28 12:56:55 +0300

    исправил ДЗ по 5 уроку

--- src/com/startjava/lesson_5/create_db.sql
+++ src/com/startjava/lesson_5/create_db.sql
@@ -3,14 +3,14 @@ DROP TABLE IF EXISTS jaegers;
 \c robots;
 
 CREATE TABLE jaegers (
-	id SERIAL PRIMARY KEY,
+	id 		   SERIAL PRIMARY KEY,
 	model_name VARCHAR(30),
-	mark VARCHAR(6),
-	height REAL,
-	weight REAL,
-	status VARCHAR(10),
-	origin VARCHAR(10),
-	launch DATE,
+	mark 	   VARCHAR(6),
+	height 	   REAL,
+	weight 	   REAL,
+	status 	   VARCHAR(10),
+	origin 	   VARCHAR(10),
+	launch 	   DATE,
 	kaiju_kill INT
 );
 
